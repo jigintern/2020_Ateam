67ad2fde37ee9365092a35edfd97b5f545e38200
shunya <67567340+shunya-maruboro@users.noreply.github.com>
2020-08-19 18:08:05 +0900

    Add files via upload

---
+++ B/main.js
@@ -0,0 +1,18 @@
+import { Server } from "https://code4sabae.github.io/js/Server.js"
+
+let port = 8881;
+
+class MyServer extends Server {
+    api(path, req) {
+        console.log(path); //要求されたpathをコンソールに表示
+        if (path === "/api/") {
+            return { res: };
+        }
+        else if (path === ) {
+            return {res: };
+        }
+        return null;
+    }
+};
+
+new MyServer(port);
