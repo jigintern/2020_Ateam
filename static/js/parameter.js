d47e3d218d9411103101740db12a8b2eeca2297a
YusukeSano <yuu131129@gmail.com>
2020-08-24 19:27:44 +0900

    Update Parameters

---
+++ static/js/parameter.js
@@ -0,0 +1,23 @@
+// <script src="./js/parameter.js" type="text/javascript" charset="utf-8"></script>
+
+let param = new Array();
+
+const ParameterGet = () => {
+    var pair = location.search.substring(1).split('&');
+    var temp;
+    for (let index in pair){
+        temp = pair[index].split("=");
+        param[temp[0]] = temp[1];
+    }
+}
+
+const move = function(place){
+    let Psend = "";
+    for(var i = 1; i < arguments.length; ++i){
+        if(!param[arguments[i]]) break;
+        Psend += ("&" + arguments[i] + "=" + param[arguments[i]]);
+    }
+    if(Psend)Psend = "?"+Psend.slice(1);
+    const path = `./${place}.html${Psend}`;
+    location.href=path;
+};
