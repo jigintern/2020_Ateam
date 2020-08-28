a63533fd0c1d90aec56407135bb822cb12b3ec40
BuildTools <unconfigured@null.spigotmc.org>
2020-08-28 12:07:49 +0900

    スマホも対応？

--- static/js/video.js
+++ static/js/video.js
@@ -1,19 +1,31 @@
 const imageScaleFactor = 0.2;
-const outputStride = 16;
-const flipHorizontal = true;
+const architecture = 'MobileNetV1'; // 'MobileNetV1'(精度:低, 速度:高) or 'ResNet50'(精度:高, 速度:低)
+const outputStride = 8; // MobileNetv1: 8, 16, 32、ResNet: 16, 32 (大きくなるほど 精度：高, 速度：低)
+const inputResolution = 200; // 入力解像度：(default:257) (大きくなるほど 精度：高, 速度：低)
+const  multiplier = mob ? 0.50 : 0.75; // (MobileNetV1のみ) 畳み込み演算の深さ（チャネル数）の浮動小数点乗数：-1.01, 1.0, 0.75, 0.50 (大きくなるほど 精度：高, 速度：低)
+const quantBytes = 2; // 重みの量子化に使用されるバイト：1, 2, 4 (大きくなるほど 精度：高, 速度：低)
+const flipHorizontal = mob ? false : true;
+const contentWidth = 800;
 const contentHeight = 600;
-const men = decodeURI(param["menu"]);
 
 async function bindPage() {
 
-    //
-    if(men != "腕立て") {
+    //腕立ての時以外はカメラ使わない
+    if(mn != "腕立て") {
         document.getElementById("wait").style.display = "none";
-        console.log(men)
+        console.log(mn)
         return;
     }
 
-    const net = await posenet.load(); // posenetの呼び出し
+    //posenetの呼び出し
+    const net = await posenet.load({
+        architecture: architecture,
+        outputStride: outputStride,
+        inputResolution: inputResolution,
+        multiplier: multiplier,
+        quantBytes: quantBytes
+    });
+
     let video;
     try {
         video = await loadVideo(); // video属性をロード
@@ -35,10 +47,20 @@ async function loadVideo() {
 // video属性からストリームを取得する
 async function setupCamera() {
     const video = document.getElementById('video');
+    video.width = contentWidth;
+    video.height = contentHeight;
+
     if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+
+        const mobile = mob;
+
         const stream = await navigator.mediaDevices.getUserMedia({
             'audio': false,
-            'video': true
+            'video': {
+                facingMode: 'user',
+                width: mobile ? undefined : contentWidth,
+                height: mobile ? undefined : contentHeight,
+            },
         });
         video.srcObject = stream;
 
@@ -66,6 +88,7 @@ function detectPoseInRealTime(video, net) {
         poses.push(pose);
 
         poses.forEach(({ score, keypoints }) => {
+            console.log(keypoints[0]);
             //最初だけ実効
             if (count === 0) {
                 //点描が可能になったので"loadingを消す"
@@ -77,8 +100,7 @@ function detectPoseInRealTime(video, net) {
             }
 
             //上から1/4以下の時(+ score(認識制度)が0.8点以上の時)(+ textが"none"でないとき)
-            console.log(keypoints[0]);
-            if ( (contentHeight*2/5) > keypoints[0].position.y && keypoints[0].score > 0.8 && (count === 1 || count === 3) ) {  //keypoints[0]には鼻の予測結果が格納されている 
+            if ( (contentHeight*2/5) > keypoints[0].position.y && keypoints[0].score > 0.7 && (count === 1 || count === 3) ) {  //keypoints[0]には鼻の予測結果が格納されている 
 
                 if (count === 3) {
                     console.log("良いね！");
@@ -94,7 +116,7 @@ function detectPoseInRealTime(video, net) {
                 count = 2;
             }
             //下から1/5以下の時
-            else if ((contentHeight* 4/5) < keypoints[0].position.y && keypoints[0].score > 0.8 && count === 2) {
+            else if ((contentHeight* 4/5) < keypoints[0].position.y && keypoints[0].score > 0.7 && count === 2) {
                 //計測経過
                 text.textContent = "ゆっくり戻しましょう"
                 document.getElementById("tex").appendChild(text);
